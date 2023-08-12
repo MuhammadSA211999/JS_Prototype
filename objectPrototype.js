db074ddf5c0ebcf1d69b86e6634073879b6406e5
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-12 07:25:38 +0600

    prototype and inheritance

---
+++ objectPrototype.js
@@ -0,0 +1,7 @@
+// JS is a object based functional programming language.
+let person={}
+person.name='MSA'
+person.eating=function(){
+    console.log(`${this.name} is eating!`);
+}
+console.log(person);
